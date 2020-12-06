77c6a14cc30d9f2aa170cae7b562592a0b374d05
jaytube <tuxiaoyue@126.com>
2020-12-06 11:51:58 +0800

    project init

---
+++ icbms-web/src/main/java/com/icbms/IcbmsSvsApplication.java
@@ -0,0 +1,13 @@
+package com.icbms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcbmsSvsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcbmsSvsApplication.class, args);
+    }
+
+}
